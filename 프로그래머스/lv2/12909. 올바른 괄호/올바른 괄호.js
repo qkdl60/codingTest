270b1ce414191f5874c76659e3eacbfb5fc1da61
qkdl60 <61609327+qkdl60@users.noreply.github.com>
2023-06-07 13:35:02 +0900

    [level 2] Title: 올바른 괄호, Time: 3.02 ms, Memory: 37.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,7 +1,7 @@
 function solution(s){
   
     let count=0;
-    if(s[0]===")")return false
+    if(s[0]===")" || s[s.length-1]==="(" || s.length%2===1)return false
     for(let i=0; i<s.length; i++){
         if(s[i]==="(")count++
         else{
