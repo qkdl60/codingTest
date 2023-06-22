4430736cbc26621d5ce9b3eb7b33baf45eaa14ec
qkdl60 <61609327+qkdl60@users.noreply.github.com>
2023-06-22 13:11:58 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,5 +1,40 @@
 function solution(s) {
-    s=s.replaceAll("zero","0").replaceAll("one","1").replaceAll("two","2").replaceAll("three","3").replaceAll("four","4").replaceAll("five","5").replaceAll("six","6").replaceAll("seven","7").replaceAll("eight","8").replaceAll("nine","9")
-    var answer = Number(s);
-    return answer;
+    const dict = {
+        'zero': '0',
+        'one': '1',
+        'two': '2',
+        'three': '3',
+        'four': '4',
+        'five': '5',
+        'six': '6',
+        'seven': '7',
+        'eight': '8',
+        'nine': '9',
+    }
+    const words = Object.keys(dict);
+    const numbers = Object.values(dict);
+    
+    const result = [];
+    
+    let charFlag = -1; // 새 영단어가 시작되는 인덱스
+    for (let i = 0; i < s.length; i++) {
+        // 숫자인 경우
+				if (numbers.includes(s[i])) {
+            result.push(s[i]);
+            charFlag = -1;
+            continue;
+        }
+				
+				// 문자열인 경우
+        if (charFlag < 0) {
+            charFlag = i;
+        }
+        const substr = s.substring(charFlag, i + 1);
+        if (words.includes(substr)) {
+            result.push(dict[substr]);
+            charFlag = -1;
+        }
+    }
+    return parseInt(result.join(''));
+    
 }
