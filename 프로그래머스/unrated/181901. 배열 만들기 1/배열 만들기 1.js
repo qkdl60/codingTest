b071919d1d6f6a3b599299873d059b45b6a8c195
qkdl60 <61609327+qkdl60@users.noreply.github.com>
2023-07-03 21:20:10 +0900

    [unrated] Title: 배열 만들기 1, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.js
+++ 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.js
@@ -1,10 +1,11 @@
 function solution(n, k) {
-    let m=k;
-    const answer=[];
-    while(m<=n){
-        answer.push(m);
-        m+=k;
-    }
+//     let m=k;
+//     const answer=[];
+//     while(m<=n){
+//         answer.push(m);
+//         m+=k;
+//     }
     
-    return answer
+//     return answer
+    return Array.from({length:Math.floor(n/k)},(_,i)=>(i+1)*k)
 }
