6021875343203d45a156ec011aee6fa50f395a21
qkdl60 <61609327+qkdl60@users.noreply.github.com>
2023-07-18 01:46:53 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,5 +1,10 @@
 function solution(s) {
-    s=s.split(" ").map(i=>+i);
+//     s=s.split(" ").map(i=>+i);
     
-    return `${Math.min(...s)} ${Math.max(...s)}`;
+//     return `${Math.min(...s)} ${Math.max(...s)}`;
+ 
+    
+      const arr = s.split(' ');
+
+    return Math.min(...arr)+' '+Math.max(...arr);
 }
