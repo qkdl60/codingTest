de5524c7483384dae9eeb5db747de93ed3d09832
qkdl60 <61609327+qkdl60@users.noreply.github.com>
2023-08-01 00:23:43 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
+++ 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
@@ -1,10 +1,34 @@
 function solution(price, money, count) {
-   let pp=0;
-    for(let i=1; i<=count; i++){
-        pp+=price*i;
-    }
+    /*이용가격 price, n번째 이용시 n*price , 놀이기구를 count번 탄다면 자신이 가지고 있느것에서       얼마가 모자란가? 부족하지않다면 0을 리턴
+    
+    이전 값에price를 해주고 모두 더한다. 
+    1부터 n까지의 합은 n*(n+1)/2 이다. 
+    
+    */
+    const total=count*(count+1)/2;
+    const totalPrice=total*price;
+    return totalPrice-money>0? totalPrice-money: 0;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+//    let pp=0;
+//     for(let i=1; i<=count; i++){
+//         pp+=price*i;
+//     }
     
     
 
-    return pp>money ? pp-money: 0
+//     return pp>money ? pp-money: 0
 }
