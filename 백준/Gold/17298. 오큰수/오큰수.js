1e56a6abf003ff3e4fe7dc63fd7ec72493037cc4
박노철 <61609327+qkdl60@users.noreply.github.com>
2024-01-06 20:29:37 +0900

    [Gold IV] Title: 오큰수, Time: 832 ms, Memory: 196840 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.js
+++ 백준/Gold/17298. 오큰수/오큰수.js
@@ -1,31 +1,17 @@
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-const [N,m] = fs.readFileSync(filePath).toString().trim().split("\n")
-const n=Number(N);
-const list= m.split(" ").map(Number).reverse();
-/* 현재 값보다 크고 가장 가까운 수 
-list 뒤집고, 
+const [N, m] = fs.readFileSync(filePath).toString().trim().split("\n");
 
+const n = Number(N);
+const list = m.split(" ").map(Number);
+const result = new Array(n).fill(-1);  // 모든 요소를 -1로 초기화
+const stack = [];
 
-*/
-//overNumStack 감소 수열
-const overNumStack=[list[0]];
-const result=[-1];
-for(let i =1; i<list.length; i++){
-    const cur=list[i];
-    while(overNumStack.length){
-        const compare=overNumStack[overNumStack.length-1];
-        if(compare>cur){
-            overNumStack.push(cur);
-            result.push(compare);
-            break;
-        }else{
-            overNumStack.pop();
-        }
+for (let i = 0; i < n; i++) {
+    while (stack.length > 0 && list[stack[stack.length - 1]] < list[i]) {
+        result[stack.pop()] = list[i];  // 스택의 top 위치에 현재 값 저장
     }
-    if(overNumStack.length===0){
-        overNumStack.push(cur);
-        result.push(-1);
-    }
+    stack.push(i);  // 인덱스를 스택에 추가
 }
-console.log(result.reverse().join(" "))
+
+console.log(result.join(" "));
