09b1d344b92e52638409dfa80ae896e46d3f7927
박노철 <61609327+qkdl60@users.noreply.github.com>
2024-01-06 19:11:58 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 75.34 ms, Memory: 149 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.js
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.js
@@ -28,7 +28,7 @@ function solution(numbers) {
             //바로 뒤 있는 수의 뒤에 있는 큰 수,
             const lastPON=postOverNum[index-1];
             
-            //바로 뒤 에 있는 수의 뒤 큰 수가 -1일 때
+        //바로 뒤 에 있는 수의 뒤 큰 수가 -1일 때
         if(lastPON===-1){
             // 바로 뒷 수의 뒤 큰 수가 -1이면, 바로 뒤에 수가 가장 크거나, 수가 없거나
             //바로 뒷 수의 뒤 큰 수가-1이고, 현재값이 바로 뒤에 있는 수보다 크거나 같다면 현재값은 가장 크므로 뒤 큰 수가 없다. 즉 -1
@@ -39,11 +39,13 @@ function solution(numbers) {
                  // 바로 뒷 수가 현재 값보다 크다면 큰 뒤 수는 바로 뒷 수
                 postOverNum.push(lastNum);
              }
+        //바로 뒤 수의 뒤 큰 수가 -1이 아닐때,    
         }else{
+            //바로 뒷 수가 현재 값보다 크다면 큰 뒤 수는 바로 뒷 수
             if(i<lastNum){
                 postOverNum.push(lastNum)
             
-              
+             // 바로 뒷 수가 현재값 보다 작거나 같으며, 뒷수의 뒤 큰수가 나보다 크다면(lastNum<=i<lastPON) 
             }else if(i<lastPON){
                 postOverNum.push(lastPON);
             }else{
