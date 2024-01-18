ff23ea22578a701f9cb1f650dc2541f43dbc4c1f
박노철 <61609327+qkdl60@users.noreply.github.com>
2024-01-19 02:49:34 +0900

    [Gold V] Title: 수 고르기, Time: 276 ms, Memory: 28896 KB -BaekjoonHub

---
+++ 백준/Gold/2230. 수 고르기/수 고르기.js
@@ -0,0 +1,25 @@
+/*
+
+
+*/
+
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const[N,...L] = fs.readFileSync(filePath).toString().trim().split("\n");
+const [n,m]=N.split(" ").map(Number);
+const list =L.map(Number).sort((a,b)=>a-b);
+
+//-1, 1 차이는  2 , 
+let left=0;
+let right=1;
+let min=Number.MAX_SAFE_INTEGER;
+while(left<=right && right< n){
+    let gap=list[right]-list[left];
+    if(gap>=m){// 갭이 크다.면 갭을 줄여준다. left를 키원잗. 
+        min=Math.min(min, gap)
+        left++
+    }else{
+        right++;
+    }
+}
+console.log(min)
