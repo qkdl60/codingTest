d1a89a8f5aa378ce0dd4426f858d7e313f4da676
박노철 <61609327+qkdl60@users.noreply.github.com>
2024-01-19 02:59:15 +0900

    [Gold V] Title: 수 고르기, Time: 276 ms, Memory: 28152 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.js
+++ 백준/Gold/2230. 수 고르기/수 고르기.js
@@ -1,8 +1,3 @@
-/*
-
-
-*/
-
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const[N,...L] = fs.readFileSync(filePath).toString().trim().split("\n");
