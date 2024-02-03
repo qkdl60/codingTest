bde28f2971caa362472c7499aa3539dcbf586c37
박노철 <61609327+qkdl60@users.noreply.github.com>
2024-02-04 00:57:10 +0900

    [Silver III] Title: N과 M (6), Time: 124 ms, Memory: 9724 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const [s,l] = fs.readFileSync(filePath).toString().trim().split("\n")
+const [n,m]= s.split(" ").map(Number);
+const list=l.split(' ').map(Number).sort((a,b)=>a-b);
+
+const result = getP(list, 0);
+console.log(result.map(i=>i.join(' ')).join('\n') )
+
+
+function getP(list ,current){
+    if(current===m-1)return list.map(i=>[i]);
+    const result=[];
+    for(let i=0; i<list.length ; i++){
+        const a=list[i];
+        const rest=list.slice(i+1)
+        const pre=getP(rest, current+1).map(i=> [a, ...i]);
+        result.push(...pre);
+    }
+    
+    return result ;
+}
