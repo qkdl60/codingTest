3ad0a9a2f70148d34a30ed7f9e935b261f2108b7
박노철 <61609327+qkdl60@users.noreply.github.com>
2024-03-16 21:50:59 +0900

    [Gold IV] Title: 최단경로, Time: 988 ms, Memory: 132492 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.js
+++ 백준/Gold/1753. 최단경로/최단경로.js
@@ -1,96 +1,88 @@
-class MinHeap {
-    constructor() {
-        this.heap = [null];
+
+
+class MinHeap{ // schema {n, w }
+    constructor(){
+        this.heap=[null];
+    }
+    swap(a,b){
+        [this.heap[a], this.heap[b]]=[this.heap[b], this.heap[a]]; 
     }
 
-    push(value) {
+    push(value){
         this.heap.push(value);
-        let currentIndex = this.heap.length - 1;
-        let parentIndex = Math.floor(currentIndex / 2);
-
-        while (parentIndex !== 0 && this.heap[parentIndex].cost > value.cost) {
-            this._swap(parentIndex, currentIndex)
-
-            currentIndex = parentIndex;
-            parentIndex = Math.floor(currentIndex / 2);
+        let currentIdx= this.heap.length-1;
+        let parentIdx= Math.floor( currentIdx/2);
+        while(parentIdx!==0 && this.heap[parentIdx].w >value.w){
+            this.swap(currentIdx, parentIdx);
+            currentIdx=parentIdx;
+            parentIdx=Math.floor(currentIdx/2);
         }
     }
-    //스키마 {node, cost }
-    pop() {
-        if (this.isEmpty()) return;
-        if (this.heap.length === 2) return this.heap.pop();
 
-        const returnValue = this.heap[1];
-        this.heap[1] = this.heap.pop();
+    pop(){
+        if(this.heap.length===1) return null;
+        if( this.heap.length===2) return this.heap.pop();
+        
+        const returnValue={...this.heap[1]};
+        this.heap[1]=this.heap.pop();
 
-        let currentIndex  = 1;
-        let leftIndex = 2;
-        let rightIndex = 3;
-        while ((this.heap[leftIndex] && this.heap[currentIndex].cost > this.heap[leftIndex].cost) || 
-               (this.heap[rightIndex] && this.heap[currentIndex].cost > this.heap[rightIndex].cost)) {
-            if (this.heap[leftIndex] === undefined) { // 왼쪽 정점이 없을 경우
-                this._swap(rightIndex, currentIndex)
-                currentIndex=rightIndex;
-            } else if (this.heap[rightIndex] === undefined) { // 오른쪽 정점이 없을 경우
-                this._swap(leftIndex, currentIndex)
-                currentIndex=leftIndex;
-            } else if (this.heap[leftIndex].cost > this.heap[rightIndex].cost) {
-                this._swap(rightIndex, currentIndex)
-                currentIndex=rightIndex;
-            } else if (this.heap[leftIndex].cost <= this.heap[rightIndex].cost) {
-                this._swap(leftIndex, currentIndex)
-                currentIndex=leftIndex;
-            }
-            leftIndex = currentIndex * 2;
-            rightIndex = currentIndex * 2 + 1;
-        }
+        let current=1;
+        let left= 2;
+        let right =3; 
 
-        return returnValue;
+        while( (this.heap[left] && this.heap[current].w > this.heap[left ].w) 
+              ||(this.heap[right] && this.heap[current].w > this.heap[right].w)
+             ){
+                if(this.heap[right] ===undefined || this.heap[left].w <this.heap[right].w){
+                    this.swap(current, left);
+                    current=left;
+                }else{
+                    this.swap(current, right);
+                    current=right;
+                }
+                left=current*2; 
+                right=left+1;
+            
+             }
+        return returnValue
     }
-
-    isEmpty() {
-        return this.heap.length === 1;
-    }
-
-    _swap(a, b) { // 편의를 위해 배열의 요소를 swap하는 함수 작성
-        [this.heap[a], this.heap[b]] = [this.heap[b], this.heap[a]];
+    isEmpty(){
+        return this.heap.length ===1;
     }
 }
-
-
-
-
 const fs = require("fs");
 const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-let [ve,start,...path] = fs.readFileSync(filePath).toString().trim().split("\n");
-let [v,e]=ve.split(" ").map(i=>Number(i));
-start=Number(start);
-path=path.map(i=>i.split(" ").map(j=>Number(j)));
-const graph=Array.from({length:v+1},()=>[])
-for(let [from, to, cost]of path){
-    graph[from].push([to ,cost]);
-}
+const [N,S,...L] = fs.readFileSync(filePath).toString().trim().split("\n");
+const [v,e]=N.split(' ').map(Number);
+const s= Number(S);
+const list =L.map(i=>i.split(" ").map(Number));
+const dist= Array.from({length:v+1}, ()=>Infinity);
+dist[0]=0;
+dist[s]=0;
+const graph=Array.from({length:v+1}, ()=>[]);
 
-const dist=Array.from({length:v+1},()=>Infinity);
-dist[start]=0;
-const heap=new MinHeap();
-heap.push({node:start, cost:0});
-while(!heap.isEmpty()){
-    const {node:current, cost:currentCost}= heap.pop();
-    if(dist[current]<currentCost)continue;
-    for(let[to, cost] of graph[current]){
-        const nextCost=cost+currentCost;
-        if(dist[to]<=nextCost)continue;
-        else{
-            dist[to]=nextCost;
-            heap.push({node:to, cost:nextCost});
-        }
-    }
+
+for(let [u,v,w] of list ){
+    graph[u].push([v, w])// to, cost
 }
-let answer="";
-for(let i=1; i<=v; i++){
-    if(dist[i]==Infinity)answer+="INF\n";
-    else answer+=dist[i]+"\n";
+const heap=new MinHeap();
+heap.push({n:s, w:0});
+
+while(!heap.isEmpty()){
+    const a= heap.pop();
+    if(dist[a.n]<a.w)continue;
+    for(let [nextN, nextNodeW] of graph[a.n]){
+        const nextWeight= dist[a.n]+nextNodeW;
+        if( dist[nextN]<=nextWeight)continue;
+        dist[nextN]=nextWeight;
+        heap.push({n:nextN, w:nextWeight})
+    }
+    
+}
+let answer=''
+for(let i=1; i<dist.length; i++){
+    answer +=dist[i]===Infinity ? 'INF\n': dist[i]+'\n'
 }
 console.log(answer)
+
 
