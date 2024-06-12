26fbf7b29de4fea587462ad2b23d49d902699b02
박노철 <61609327+qkdl60@users.noreply.github.com>
2024-06-12 18:03:57 +0900

    [goormlevel] Title: 장마, Time: 0.87 ms, Memory: 43.11 MB, Difficulty: 2 -BaekjoonHub

--- goormlevel/194982/1. 장마/장마.js
+++ goormlevel/194982/1. 장마/장마.js
@@ -17,6 +17,7 @@ for(let j =0; j<r.length; j++ ){
 	const [s,e]=r[j];
 	for(let i =s-1; i<=e-1; i++){
 		l[i]++;
+		//피가 온 곳은 set으로 기록해둔다. 얼만큼 왔는지는 중요하지 않다. 왔냐, 안왔냐가 중요해서 set으로 
 		lastRain.add(i);
 	}
 	if((j+1)%3===0){
@@ -24,6 +25,7 @@ for(let j =0; j<r.length; j++ ){
 		for(let a of [...lastRain]){
 			l[Number(a)]--;
 		}
+		//최근 3일이내의 기록은 필요없다. clear
 		lastRain.clear()
 	}
 }
