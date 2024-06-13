0a11e08528dc946608f20bb8b3b2730173e5b1cc
박노철 <61609327+qkdl60@users.noreply.github.com>
2024-06-13 09:55:27 +0900

    [Silver III] Title: iSharp, Time: 120 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Silver/3568. iSharp/iSharp.js
+++ 백준/Silver/3568. iSharp/iSharp.js
@@ -1,27 +1,15 @@
-/*
- 여러 변수 정의 , 공통 변수형 변수이름추가변수형, 
-
-*/
-const fs = require('fs');
-const filePath= process.platform==='linux'?'/dev/stdin':'./input.txt';
-const [common, ...s] =fs.readFileSync(filePath).toString().trim().split(" ");
-
-const answer= [];
-
-//common+뒤에 변수형 변수이름
-//뒤에 있는 변수형 뒤집어야한다.  변수형은 3가지 
-const typeMap={'[': '[]', '*':'*', '&':'&', ']':''};
-const types=Object.keys(typeMap);
-
-for(let a of s){
-    const splited=a.split("");
-    const name=splited.filter(v=>!types.includes(v) && v!==',' && v!==';' ).join('');
-    const post=splited.reduce((acc,cur)=>{
-        if(cur==="]")return acc;
-        acc.push(typeMap[cur])
+const fs= require('fs');
+const filePath=process.platform='linux'?'/dev/stdin':'./input.txt';
+const s=fs.readFileSync(filePath).toString().trim().replace(';', '').split(', ');
+let base;
+[base, s[0]]=s[0].split(' ');
+const answer= s.map(i=>{
+    const name=i.split('').filter(j=>(j!=='&' && j!=='[' && j!==']' && j!=='*')).join('');
+    const type=i.split('').reduce((acc,cur)=>{
+        if(cur==='[')acc.push('[]')
+        if(cur==='&' || cur==='*')acc.push(cur);
         return acc;
-    },[]).reverse().join('');
-    answer.push(`${common}${post} ${name};\n`);
-}
-console.log(answer.join(''))
-//예외사항 변수명이 한글자 보장아닌가?
+    }, []).reverse().join("")
+    return `${base}${type} ${name};`;
+})
+console.log(answer.join('\n'))
