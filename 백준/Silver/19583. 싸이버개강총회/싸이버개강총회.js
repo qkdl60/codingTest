4d0727a4b010a2c00a7d2f66a991ee9d323362f6
박노철 <61609327+qkdl60@users.noreply.github.com>
2024-06-22 10:29:34 +0900

    [Silver II] Title: 싸이버개강총회, Time: 448 ms, Memory: 49060 KB -BaekjoonHub

---
+++ 백준/Silver/19583. 싸이버개강총회/싸이버개강총회.js
@@ -0,0 +1,41 @@
+/*
+    개강 총회 시작 전: 입장 여부 확인, 시작 이전에 채팅 닉넴임으로 
+    
+    끝내고: 총회 끝난 이 후 채팅 닉네임으로 체크 
+
+
+
+모든 시간 분 단위로 변환 
+
+채팅기록 100000 으로 
+
+출석이 확인된 학회원의 인원 수를 출력한다.
+*/
+const fs =require('fs');
+const filePath=process.platform==='linux'?'/dev/stdin':'./input.txt';
+const [T,...S]= fs.readFileSync(filePath).toString().trim().split('\n');
+const [startTime, endTime, streamEndTime]=T.split(' ');
+const startMin=getTimePerMin(startTime);
+const endMin=getTimePerMin(endTime);
+const streamMin=getTimePerMin(streamEndTime);
+
+const beforeStart=new Set;
+const afterEnd=new Set;
+for(let a of S){
+    const [tStr, name]=a.split(' ');
+    const time=getTimePerMin(tStr);
+    if(time<=startMin)beforeStart.add(name);
+    if(time>=endMin && time<=streamMin)afterEnd.add(name);
+}
+let count=0;
+for(let a of beforeStart){
+    if(afterEnd.has(a))count++;
+}
+console.log(count)
+
+
+
+function getTimePerMin(str){
+    const [h,m]=str.split(":").map(Number);
+    return (h*60)+m
+}
